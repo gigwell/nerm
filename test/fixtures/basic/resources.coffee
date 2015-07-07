0966f0f0882ea6c308ac7496e9eb36f2cc9a99ea
Matt Roman <romanmt@gmail.com>
2015-07-07 14:17:14 -0400

    Basic GET

---
+++ test/fixtures/basic/resources.coffee
@@ -0,0 +1,12 @@
+ObjectId = require('mongoose').Types.ObjectId
+
+module.exports = [
+  {
+    _id: ObjectId("ffffffffffffa00000000001")
+    name: "Best Resource"
+  },
+  {
+    _id: ObjectId("ffffffffffffa00000000002"),
+    name: "Worst Resource"
+  }
+]
--- test/nerm.test.js
+++ test/nerm.test.js
@@ -1,7 +1,54 @@
+require("coffee-script/register")
 require('should')
 
-describe("NERM", function() {
-  it("is a dummy test", function() {
-    "cool".should.eql("cool")
+var express = require('express'),
+    fixtureConf = require("./fixtures/config"),
+    fixtures = require('mongo-fixtures'),
+    mongoose = require('mongoose'),
+    Nerm = require(".."),
+    app = express(),
+    supertest = require('supertest')
+
+mongoose.connect("mongodb://33.33.33.100:27017/nerm-test")
+var Resource = mongoose.model('Resource', new mongoose.Schema({ name: String }))
+
+Nerm.route(app, Resource)
+app.listen(5050)
+
+var request = supertest.agent(app)
+
+before(function(done) {
+  fixtures(fixtureConf).load({
+    env: 'test',
+    username: null,
+    password: null,
+    dataset: 'basic'
+  }, done)
+});
+
+describe("Nerm", function() {
+  describe("GET", function() {
+    it("returns all objects of the specified type", function(done) {
+      request.get('/api/v0/resources')
+        .expect(200)
+        .expect(function(res) {
+          res.body.resources.should.match([
+              {_id: "ffffffffffffa00000000001", name: "Best Resource"},
+              {_id: "ffffffffffffa00000000002", name: "Worst Resource"}
+          ])
+        })
+        .end(done)
+    })
+
+    it("returns an individual item", function(done) {
+      request.get('/api/v0/resources/ffffffffffffa00000000002')
+        .expect(200)
+        .expect(function(res) {
+          res.body.resource.should.have.properties({
+            name: "Worst Resource"
+          })
+        })
+        .end(done)
+    })
   })
 })
